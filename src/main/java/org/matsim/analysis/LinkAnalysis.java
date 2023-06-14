5b502b86bf1e10e90d15a26392167efd1eb17dd7
aryana0206 <aryana.zakiya@gmail.com>
2023-06-14 16:51:57 +0200

    event handler to analyse link added

---
+++ src/main/java/org/matsim/analysis/LinkAnalysis.java
@@ -0,0 +1,16 @@
+package org.matsim.analysis;
+
+import org.matsim.core.events.EventsUtils;
+
+public class LinkAnalysis {
+
+    public static void main(String[] args) {
+
+        var handler = new LinkEventHandler();
+        var manager = EventsUtils.createEventsManager();
+        manager.addHandler(handler);
+
+        // here is a change
+        EventsUtils.readEvents(manager, "C:\\Users\\zakiyaap\\IdeaProjects\\matsim-serengeti-park-hodenhagen\\scenarios\\serengeti-park-v1.0\\output\\output-serengeti-park-v1.0-run1\\serengeti-park-v1.0-run1.output_events.xml.gz");
+    }
+}
